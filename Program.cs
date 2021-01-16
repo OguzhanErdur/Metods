ea92ab8d834c22f1372b0fe711a4056c9b6a3297
Oğuzhan ERDUR <Ouzhan ERDUR@ERDUR>
2021-01-16 23:45:58 +0300

    3. ders sonu

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Metods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { "Elma, Karpuz" };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2, };
+
+
+            //type-safe -- tip güvenli
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------------");
+
+
+
+            }
+
+            Console.WriteLine("--------Metods--------");
+
+            //instance - örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Sulu armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 15, 8);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır", 80, 5);
+
+        }
+    }
+}
+
+
+
+
+
+
+//metodlar - tekrar tekrar kullanılan şeyler için
+//Don't repeat yourself!!!!!! - DRY  - clean code - Best practice (en iyi kod yazma teknikleri)
+
+
